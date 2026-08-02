0806eb84050946ef77e329771412a769e6f8e960
Aarya Kulkarni <aarya.sk@somaiya.edu>
2026-08-02 19:16:54 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int num:nums){
+            mp[num]++;
+        }
+        vector<int>res;
+        for(auto &it:mp){
+            if(it.second>nums.size()/3){
+                res.push_back(it.first);
+            }
+        }
+        return res;
+
+    }
+};
